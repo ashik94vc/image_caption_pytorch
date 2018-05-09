a2076351a146ac2cadc9e273ac2ef431fa68db2a
Ashik Vetrivelu <vcashik@gmail.com>
2018-05-09 03:24:50 -0400

    Added Progress Bar

--- core/classifier.py
+++ core/classifier.py
@@ -5,6 +5,9 @@ from torch.backends import cudnn
 from torch.utils.data import DataLoader
 
 from torchvision import transforms
+
+from tqdm import tqdm
+
 from dataset.imagenet_dataset import ImagenetDataset
 from core.resnet import ResNet
 
@@ -15,14 +18,15 @@ class Classifier(object):
     """
 
     def __init__(self):
-        train_transform = transforms.Compose({
+        train_transform = transforms.Compose([
+            transforms.ToPILImage(),
             transforms.RandomHorizontalFlip(),
             transforms.RandomRotation(15),
             transforms.ToTensor()
-        })
-        test_transform = transforms.Compose({
+        ])
+        test_transform = transforms.Compose([
             transforms.ToTensor()
-        })
+        ])
 
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         print("Using device %s" % self.device)
@@ -41,12 +45,18 @@ class Classifier(object):
             cudnn.benchmark = True
 
     def train(self, epoch):
-        print('\nEpoch %d ' % epoch)
         self.net.train()
         criterion = nn.CrossEntropyLoss()
         optimizer = optim.SGD(self.net.parameters(), 0.001, momentum=0.9, weight_decay=5e-4)
         train_loss = 0
+        correct = 0
+        total = 0
+        acc = 0
+        barformat = "{desc}{percentage:3.0f}%|{bar}|{n_fmt}/{total_fmt}\
+        [{remaining} {rate_fmt} {postfix}%]"
+        pbar = tqdm(total=len(self.trainloader), bar_format=barformat, postfix={"accuracy":acc})
         for batch_idx, (data, target) in enumerate(self.trainloader):
+            pbar.set_description("Epoch {0} training".format(epoch+1))
             data, target = data.to(self.device), target.to(self.device)
             optimizer.zero_grad()
             outputs = self.net(data)
@@ -54,7 +64,13 @@ class Classifier(object):
             loss.backward()
             optimizer.step()
             train_loss += loss.item()
-            print(batch_idx, train_loss)
+            _, predicted = outputs.max(1)
+            total += target.size(0)
+            correct += predicted.eq(target).sum().item()
+            acc = 100.0*correct/total
+            pbar.set_postfix({"accuracy":acc})
+            pbar.update(1)
+            # print(batch_idx, train_loss)
 
         print("\nEpoch %d complete... " % epoch)
 
