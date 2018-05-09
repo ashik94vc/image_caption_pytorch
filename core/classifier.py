3d22562933da002551dda768dcddc21a2102d2e8
Ashik Vetrivelu <vcashik@gmail.com>
2018-05-09 05:26:00 -0400

    Added ImageDataGenerator from Keras

--- core/classifier.py
+++ core/classifier.py
@@ -10,6 +10,8 @@ from tqdm import tqdm
 
 from dataset.imagenet_dataset import ImagenetDataset
 from core.resnet import ResNet
+from core.image import ImageDataGenerator
+from core.lib import loadData
 
 
 class Classifier(object):
@@ -18,26 +20,28 @@ class Classifier(object):
     """
 
     def __init__(self):
-        train_transform = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.RandomHorizontalFlip(),
-            transforms.RandomRotation(15),
-            transforms.ToTensor()
-        ])
-        test_transform = transforms.Compose([
-            transforms.ToTensor()
-        ])
-
+        
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         print("Using device %s" % self.device)
 
-        train_dataset = ImagenetDataset(train=True, transform=train_transform)
-        self.classes = train_dataset.class_names
-        self.trainloader = \
-        DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=2)
-        test_dataset = ImagenetDataset(train=True, transform=test_transform)
-        self.testloader = DataLoader(test_dataset, batch_size=32, shuffle=True, num_workers=2)
-
+        # train_dataset = ImagenetDataset(train=True, transform=train_transform)
+        # self.classes = train_dataset.class_names
+        # self.trainloader = \
+        # DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=2)
+        # test_dataset = ImagenetDataset(train=True, transform=test_transform)
+        # self.testloader = DataLoader(test_dataset, batch_size=32, shuffle=True, num_workers=2)
+        dataset = loadData("dataset/imagenet.h5")
+        imagegen = ImageDataGenerator(shear_range=0.2,zoom_range=0.2,horizontal_flip=True)
+        train_x = dataset["train"]["data"]
+        train_y = dataset["train"]["target"]
+        self.train_len = len(train_x)
+        self.trainloader = imagegen.flow(train_x, train_y,batch_size=32,shuffle=True)
+        test_x = dataset["val"]["data"]
+        test_y = dataset["val"]["target"]
+        self.test_len = len(test_x)
+        self.testloader = imagegen.flow(test_x, test_y,batch_size=32,shuffle=True)
+        self.classes = dataset["class_index"]
+        
         self.net = ResNet(len(self.classes))
         self.net.to(self.device)
         if self.device == 'cuda':
@@ -52,12 +56,11 @@ class Classifier(object):
         correct = 0
         total = 0
         acc = 0
-        barformat = "{desc}{percentage:3.0f}%|{bar}|{n_fmt}/{total_fmt}\
-        [{remaining} {rate_fmt} {postfix}%]"
-        pbar = tqdm(total=len(self.trainloader), bar_format=barformat, postfix={"accuracy":acc})
+        barformat = "{desc}{percentage:3.0f}%|{bar}|{n_fmt}/{total_fmt} {postfix[loss]}[{remaining} {rate_fmt} accuracy:{postfix[accuracy]}%]"
+        pbar = tqdm(total=self.train_len, bar_format=barformat, postfix={"accuracy":acc, "loss":0, 5:0})
         for batch_idx, (data, target) in enumerate(self.trainloader):
-            pbar.set_description("Epoch {0} training".format(epoch+1))
-            data, target = data.to(self.device), target.to(self.device)
+            pbar.set_description("epoch {0}".format(epoch+1))
+            data, target = torch.tensor(data, dtype=torch.float32, device=self.device), torch.tensor(target, dtype=torch.int64, device=self.device)
             optimizer.zero_grad()
             outputs = self.net(data)
             loss = criterion(outputs, target)
@@ -68,8 +71,15 @@ class Classifier(object):
             total += target.size(0)
             correct += predicted.eq(target).sum().item()
             acc = 100.0*correct/total
-            pbar.set_postfix({"accuracy":acc})
-            pbar.update(1)
+            pbar.postfix["accuracy"] = "{0:.2f}".format(acc)
+            pbar.postfix["loss"] = "{0:10.3f}".format(train_loss)
+            if self.device == "cuda":
+                if batch_idx % 50 == 0:
+                    pbar.update(50)
+            else:
+                pbar.update(1)
+            if batch_idx >= self.train_len:
+                break
             # print(batch_idx, train_loss)
 
         print("\nEpoch %d complete... " % epoch)
@@ -78,7 +88,10 @@ class Classifier(object):
         self.net.eval()
         correct = 0
         total = 0
+        acc = 0
         with torch.no_grad():
+            barformat = "{desc}{percentage:3.0f}%|{bar}|{n_fmt}/{total_fmt}[{remaining} {rate_fmt} {postfix}%]"
+            pbar = tqdm(total=self.train_len, bar_format=barformat, postfix={"accuracy":acc})
             for _, (data, target) in enumerate(self.testloader):
                 data, target = data.to(self.device), target.to(self.device)
                 outputs = self.net(data)
@@ -86,9 +99,10 @@ class Classifier(object):
                 _, predicted = outputs.max(1)
                 total += target.size(0)
                 correct += predicted.eq(target).sum().item()
-        acc = 100.*correct/total
-        print("\nEpoch %d complete with test accuracy %f" % (epoch, acc))
-        print("Saving Model")
+                acc = 100.*correct/total
+                pbar.set_postfix({"accuracy":acc})
+                pbar.update(1)
+        print("Saving Model...")
         state = {
             'net': self.net.state_dict(),
             'epoch': epoch,
--- dataset/imagenet_dataset.py
+++ dataset/imagenet_dataset.py
@@ -24,13 +24,12 @@ class ImagenetDataset(Dataset):
         return len(self.dataset['target'])
 
     def __getitem__(self, idx):
-        sample_data = []
+        sample_data = tuple()
         image_data = self.dataset['data'][idx]
         # image_data = image_data.transpose(1, 2, 0)
         # image_data = Image.fromarray(image_data)
         if self.transform:
             image_data = self.transform(image_data)
-        sample_data.append(image_data)
-        sample_data.append(self.dataset['target'][idx])
+        sample_data = (image_data, self.dataset['target'][idx])
 
         return sample_data
