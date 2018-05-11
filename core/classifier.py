abedf8c9a6814bfe56c030233698098dff6c1342
Ashik Vetrivelu <vcashik@gmail.com>
2018-05-11 18:06:41 -0400

    Fixed resize transform

--- core/classifier.py
+++ core/classifier.py
@@ -33,14 +33,14 @@ class Classifier(object):
         print("Using device %s" % self.device)
 
         train_transform = transforms.Compose([
-            transforms.Resize(256),
+            transforms.Resize((256,256)),
             transforms.RandomCrop(224),
             transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
         ])
         test_transform = transforms.Compose([
-            transforms.Resize(224),
+            transforms.Resize((224,224)),
             transforms.ToTensor(),
             transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225))
         ])
